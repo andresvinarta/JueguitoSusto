840379e531b916058050603771c812b7d49f9f55
Andrés Viñarta <andresvinarta@gmail.com>
2024-11-04 19:16:27 +0100

    Añadidos sonidos al caminar
    Ahora se sincronizan sonidos de caminar con la oscilacion de la camara

--- Assets/Scripts/Miisc/ObjectSwing.cs
+++ Assets/Scripts/Miisc/ObjectSwing.cs
@@ -1,3 +1,4 @@
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class ObjectSwing : MonoBehaviour
@@ -15,12 +16,19 @@ public class ObjectSwing : MonoBehaviour
     private float TimerOff = 0;
     public float ReturnDuration = 100f;
 
+    [SerializeField]
+    private GameObject PlayerStepsManager;
+
+    private AudioSource[] StepSounds;
+
+    private bool JustStepped;
 
     private void Awake()
     {
         StartPos = this.transform.localPosition;
         PlayerInput = new InputSystem_Actions();
         PlayerInput.Enable();
+        StepSounds = PlayerStepsManager.GetComponents<AudioSource>();
     }
 
 
@@ -42,6 +50,7 @@ public class ObjectSwing : MonoBehaviour
                     transform.localPosition = Vector3.Lerp(transform.localPosition, StartPos, TimerOff/ReturnDuration);
                     TimerOff += Time.deltaTime;
                     TimerOn = 0;
+                    JustStepped = false;
                 }
                 //Debug.Log(Player.GetComponent<Rigidbody>().angularVelocity.magnitude.ToString());
                 //Debug.Log((Player.transform.position - PlayerLastPos).magnitude.ToString());
@@ -56,6 +65,17 @@ public class ObjectSwing : MonoBehaviour
 
         Vector3 NewPos = StartPos;
         NewPos.y += Swing;
+
+        if (!JustStepped && Swing <= -Amplitude + 0.015f)
+        {
+            int RandomNumber = UnityEngine.Random.Range((int)0, (int)StepSounds.Length);
+            StepSounds[RandomNumber].Play();
+            JustStepped = true;
+        }
+        else if (Swing > -Amplitude + 0.015f)
+        {
+            JustStepped = false;
+        }
 
         transform.localPosition = NewPos;
     }
